8aaa904e714e883d5af147a448d562f9fd1f98e1
si185047 <si185047@WINSI185047-ZH4.corp.ncr.com>
2018-04-24 09:46:19 +0530

    adding auto rotate images

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,6 @@
 var slideIndex = 0;
-showSlides(slideIndex);
+showSlides();
+//showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -25,4 +26,19 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+}
+
+
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
