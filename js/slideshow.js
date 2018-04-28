ecd60235c7e01ad79ce75fe987c1212586e31c6a
si185047 <si185047@WINSI185047-ZH4.corp.ncr.com>
2018-04-28 15:26:13 +0530

    added gallery

--- js/slideshow.js
+++ js/slideshow.js
@@ -39,6 +39,6 @@ function showSlides() {
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
+    slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
